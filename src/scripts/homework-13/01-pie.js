5edaf2b51feec5684e49a1ee0820e19e66f5d8f7
fr48 <fr48@columbia.edu>
2019-10-22 00:36:54 -0400

    cleaned up first two scripts

--- src/scripts/homework-13/01-pie.js
+++ src/scripts/homework-13/01-pie.js
@@ -4,10 +4,63 @@ const margin = { top: 30, left: 30, right: 30, bottom: 30 }
 const height = 400 - margin.top - margin.bottom
 const width = 780 - margin.left - margin.right
 
-// At the very least you'll need scales, and
-// you'll need to read in the file. And you'll need
-// and svg, too, probably.
+// add a svg
+const svg = d3
+  .select('#chart-1')
+  .append('svg')
+  .attr('height', height + margin.top + margin.bottom)
+  .attr('width', width + margin.left + margin.right)
+  .append('g')
+  .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+  .attr('transform', `translate(${width / 2},${height / 2})`)
 
+// need some scales!
+const pie = d3.pie().value(function(d) {
+  return d.minutes
+})
+
+const radius = 150
+
+const labelArc = d3
+  .arc()
+  .innerRadius(0)
+  .outerRadius(radius * 2 + 10)
+
+const arc = d3
+  .arc()
+  .innerRadius(0)
+  .outerRadius(radius)
+
+const colorScale = d3.scaleOrdinal().range(['purple', 'green', 'orange'])
+
+// get the file
+d3.csv(require('/data/time-breakdown.csv')).then(ready)
+// ready to start
 function ready(datapoints) {
-  
+  // console.log(datapoints)
+  svg
+    .selectAll('path')
+    .data(pie(datapoints))
+    .enter()
+    .append('path')
+    .attr('d', d => arc(d))
+    .attr('fill', d => colorScale(d.data.task))
+
+  svg
+    .selectAll('.pie-labels')
+    .data(pie(datapoints))
+    .enter()
+    .append('text')
+    .text(d => d.data.task)
+    .attr('transform', function(d) {
+      return 'translate(' + labelArc.centroid(d) + ')'
+    })
+    .attr('text-anchor', function(d) {
+      if (d.startAngle > Math.PI) {
+        return 'end'
+      } else {
+        return 'start'
+      }
+    }) // center the text on it's origin
+    .style('fill', 'Purple')
 }
